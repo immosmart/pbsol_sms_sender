0809885028250fce7ee640e025e6be6199c68531
smorodin <smorodin@immo.ru>
2013-12-16 15:13:38 +0400

    fix smsGatewayLog

--- models/PbsolSmsLog.php
+++ models/PbsolSmsLog.php
@@ -16,94 +16,93 @@
  */
 class PbsolSmsLog extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return PbsolSmsLog the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
-	
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'pbsol_sms_log';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return PbsolSmsLog the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('guid, from, to, status', 'length', 'max'=>255),
-			array('text, is_wait, created, delivered', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, guid, from, to, text, status, is_wait, created, delivered', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'pbsol_sms_log';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('guid, from, to, status', 'length', 'max' => 255),
+            array('text, is_wait, created, delivered', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, guid, from, to, text, status, is_wait, created, delivered', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'guid' => 'Guid',
-			'from' => 'From',
-			'to' => 'To',
-			'text' => 'Text',
-			'status' => 'Status',
-			'is_wait' => 'Is Wait',
-			'created' => 'Created',
-			'delivered' => 'Delivered',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'guid' => 'Guid',
+            'from' => 'From',
+            'to' => 'To',
+            'text' => 'Text',
+            'status' => 'Status',
+            'is_wait' => 'Is Wait',
+            'created' => 'Created',
+            'delivered' => 'Delivered',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('guid',$this->guid,true);
-		$criteria->compare('t.from',$this->from,true);
-		$criteria->compare('t.to',$this->to,true);
-		$criteria->compare('text',$this->text,true);
-		$criteria->compare('status',$this->status,true);
-		$criteria->compare('is_wait',$this->is_wait);
-		$criteria->compare('created',$this->created,true);
-		$criteria->compare('delivered',$this->delivered,true);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id);
+        $criteria->compare('guid', $this->guid, true);
+        $criteria->compare('t.from', $this->from, true);
+        $criteria->compare('t.to', $this->to, true);
+        $criteria->compare('text', $this->text, true);
+        $criteria->compare('status', $this->status, true);
+        $criteria->compare('is_wait', $this->is_wait);
+        $criteria->compare('created', $this->created, true);
+        $criteria->compare('delivered', $this->delivered, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 
     /**
      * Push sms
